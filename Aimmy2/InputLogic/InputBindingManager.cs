9a5c7636bc9d9e3665f5b869a78433f98adbc9fa
apraxo <114722326+apraxo@users.noreply.github.com>
2024-06-24 01:17:48 -0600

    Add files via upload

--- Aimmy2/InputLogic/InputBindingManager.cs
+++ Aimmy2/InputLogic/InputBindingManager.cs
@@ -1,47 +1,24 @@
-﻿using Gma.System.MouseKeyHook;
-using System.Windows.Forms;
-
 namespace InputLogic
 {
     internal class InputBindingManager
     {
-        private IKeyboardMouseEvents? _mEvents;
-        private readonly Dictionary<string, string> bindings = [];
-        private static readonly Dictionary<string, bool> isHolding = [];
-        private string? settingBindingId = null;
+        // Existing code ...
 
-        public event Action<string, string>? OnBindingSet;
+        private readonly Dictionary<string, string> actionBindings = new();
 
-        public event Action<string>? OnBindingPressed;
-
-        public event Action<string>? OnBindingReleased;
-
-        public static bool IsHoldingBinding(string bindingId) => isHolding.TryGetValue(bindingId, out bool holding) && holding;
-
-        public void SetupDefault(string bindingId, string keyCode)
+        public void SetupDefaultAction(string actionId, string keyCode, string action)
         {
-            bindings[bindingId] = keyCode;
-            isHolding[bindingId] = false;
-            OnBindingSet?.Invoke(bindingId, keyCode);
+            bindings[actionId] = keyCode;
+            actionBindings[actionId] = action;
+            isHolding[actionId] = false;
+            OnBindingSet?.Invoke(actionId, keyCode);
             EnsureHookEvents();
         }
 
-        public void StartListeningForBinding(string bindingId)
+        public void StartListeningForActionBinding(string actionId)
         {
-            settingBindingId = bindingId;
+            settingBindingId = actionId;
             EnsureHookEvents();
-        }
-
-        private void EnsureHookEvents()
-        {
-            if (_mEvents == null)
-            {
-                _mEvents = Hook.GlobalEvents();
-                _mEvents.KeyDown += GlobalHookKeyDown!;
-                _mEvents.MouseDown += GlobalHookMouseDown!;
-                _mEvents.KeyUp += GlobalHookKeyUp!;
-                _mEvents.MouseUp += GlobalHookMouseUp!;
-            }
         }
 
         private void GlobalHookKeyDown(object sender, KeyEventArgs e)
@@ -49,6 +26,7 @@ namespace InputLogic
             if (settingBindingId != null)
             {
                 bindings[settingBindingId] = e.KeyCode.ToString();
+                actionBindings[settingBindingId] = "SinglePress"; // Default action
                 OnBindingSet?.Invoke(settingBindingId, e.KeyCode.ToString());
                 settingBindingId = null;
             }
@@ -60,6 +38,15 @@ namespace InputLogic
                     {
                         isHolding[binding.Key] = true;
                         OnBindingPressed?.Invoke(binding.Key);
+
+                        if (binding.Key == "AutoTriggerToggle")
+                        {
+                            MouseManager.AutoTriggerEnabled = !MouseManager.AutoTriggerEnabled;
+                        }
+                        else
+                        {
+                            MouseManager.PerformKeyAction(binding.Value, actionBindings[binding.Key]);
+                        }
                     }
                 }
             }
@@ -70,6 +57,7 @@ namespace InputLogic
             if (settingBindingId != null)
             {
                 bindings[settingBindingId] = e.Button.ToString();
+                actionBindings[settingBindingId] = "SinglePress"; // Default action
                 OnBindingSet?.Invoke(settingBindingId, e.Button.ToString());
                 settingBindingId = null;
             }
@@ -81,46 +69,20 @@ namespace InputLogic
                     {
                         isHolding[binding.Key] = true;
                         OnBindingPressed?.Invoke(binding.Key);
+
+                        if (binding.Key == "AutoTriggerToggle")
+                        {
+                            MouseManager.AutoTriggerEnabled = !MouseManager.AutoTriggerEnabled;
+                        }
+                        else
+                        {
+                            MouseManager.PerformKeyAction(binding.Value, actionBindings[binding.Key]);
+                        }
                     }
                 }
             }
         }
 
-        private void GlobalHookKeyUp(object sender, KeyEventArgs e)
-        {
-            foreach (var binding in bindings)
-            {
-                if (binding.Value == e.KeyCode.ToString())
-                {
-                    isHolding[binding.Key] = false;
-                    OnBindingReleased?.Invoke(binding.Key);
-                }
-            }
-        }
-
-        private void GlobalHookMouseUp(object sender, MouseEventArgs e)
-        {
-            foreach (var binding in bindings)
-            {
-                if (binding.Value == e.Button.ToString())
-                {
-                    isHolding[binding.Key] = false;
-                    OnBindingReleased?.Invoke(binding.Key);
-                }
-            }
-        }
-
-        public void StopListening()
-        {
-            if (_mEvents != null)
-            {
-                _mEvents.KeyDown -= GlobalHookKeyDown!;
-                _mEvents.MouseDown -= GlobalHookMouseDown!;
-                _mEvents.KeyUp -= GlobalHookKeyUp!;
-                _mEvents.MouseUp -= GlobalHookMouseUp!;
-                _mEvents.Dispose();
-                _mEvents = null;
-            }
-        }
+        // Existing code for KeyUp and MouseUp handlers ...
     }
 }
--- Aimmy2/InputLogic/MouseManager.cs
+++ Aimmy2/InputLogic/MouseManager.cs
@@ -1,197 +1,50 @@
-using Aimmy2.Class;
-using Aimmy2.MouseMovementLibraries.GHubSupport;
-using Class;
-using MouseMovementLibraries.ddxoftSupport;
-using MouseMovementLibraries.RazerSupport;
-using MouseMovementLibraries.SendInputSupport;
-using System.Drawing;
-using System.Runtime.InteropServices;
-
 namespace InputLogic
 {
     internal class MouseManager
     {
-        private static readonly double ScreenWidth = WinAPICaller.ScreenWidth;
-        private static readonly double ScreenHeight = WinAPICaller.ScreenHeight;
+        // Existing code ...
 
-        private static DateTime LastClickTime = DateTime.MinValue;
-        private static int LastAntiRecoilClickTime = 0;
-
-        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
-        private const uint MOUSEEVENTF_LEFTUP = 0x0004;
-        private const uint MOUSEEVENTF_MOVE = 0x0001;
-        private static double previousX = 0;
-        private static double previousY = 0;
-        public static double smoothingFactor = 0.5;
-        public static bool IsEMASmoothingEnabled = false;
+        public static bool AutoTriggerEnabled { get; set; } = false;
+        public static string TargetDetectedKey { get; set; } = "LButton"; // Default to left mouse button
+        public static string TargetDetectedAction { get; set; } = "SinglePress"; // Default action
 
         [DllImport("user32.dll")]
-        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
 
-        private static Random MouseRandom = new();
+        private const int KEYEVENTF_KEYDOWN = 0x0000;
+        private const int KEYEVENTF_KEYUP = 0x0002;
 
-        private static Point CubicBezier(Point start, Point end, Point control1, Point control2, double t)
+        private static void PerformKeyAction(string key, string action)
         {
-            double u = 1 - t;
-            double tt = t * t;
-            double uu = u * u;
-
-            double x = uu * u * start.X + 3 * uu * t * control1.X + 3 * u * tt * control2.X + tt * t * end.X;
-            double y = uu * u * start.Y + 3 * uu * t * control1.Y + 3 * u * tt * control2.Y + tt * t * end.Y;
-
-            if (IsEMASmoothingEnabled)
+            byte keyCode = (byte)Enum.Parse(typeof(Keys), key);
+            switch (action)
             {
-                x = EmaSmoothing(previousX, x, smoothingFactor);
-                y = EmaSmoothing(previousY, y, smoothingFactor);
-            }
-
-            return new Point((int)x, (int)y);
-        }
-
-        private static double EmaSmoothing(double previousValue, double currentValue, double smoothingFactor) => (currentValue * smoothingFactor) + (previousValue * (1 - smoothingFactor));
-
-        public static async Task DoTriggerClick()
-        {
-            int timeSinceLastClick = (int)(DateTime.UtcNow - LastClickTime).TotalMilliseconds;
-            int triggerDelayMilliseconds = (int)(Dictionary.sliderSettings["Auto Trigger Delay"] * 1000);
-            const int clickDelayMilliseconds = 20;
-
-            if (timeSinceLastClick < triggerDelayMilliseconds && LastClickTime != DateTime.MinValue)
-            {
-                return;
-            }
-
-            string mouseMovementMethod = Dictionary.dropdownState["Mouse Movement Method"];
-            Action mouseDownAction;
-            Action mouseUpAction;
-
-            switch (mouseMovementMethod)
-            {
-                case "SendInput":
-                    mouseDownAction = () => SendInputMouse.SendMouseCommand(MOUSEEVENTF_LEFTDOWN);
-                    mouseUpAction = () => SendInputMouse.SendMouseCommand(MOUSEEVENTF_LEFTUP);
+                case "SinglePress":
+                    keybd_event(keyCode, 0, KEYEVENTF_KEYDOWN, 0);
+                    keybd_event(keyCode, 0, KEYEVENTF_KEYUP, 0);
                     break;
-
-                case "LG HUB":
-                    mouseDownAction = () => LGMouse.Move(1, 0, 0, 0);
-                    mouseUpAction = () => LGMouse.Move(0, 0, 0, 0);
+                case "DoublePress":
+                    keybd_event(keyCode, 0, KEYEVENTF_KEYDOWN, 0);
+                    keybd_event(keyCode, 0, KEYEVENTF_KEYUP, 0);
+                    keybd_event(keyCode, 0, KEYEVENTF_KEYDOWN, 0);
+                    keybd_event(keyCode, 0, KEYEVENTF_KEYUP, 0);
                     break;
-
-                case "Razer Synapse (Require Razer Peripheral)":
-                    mouseDownAction = () => RZMouse.mouse_click(1);
-                    mouseUpAction = () => RZMouse.mouse_click(0);
+                case "Hold":
+                    keybd_event(keyCode, 0, KEYEVENTF_KEYDOWN, 0);
                     break;
-
-                case "ddxoft Virtual Input Driver":
-                    mouseDownAction = () => DdxoftMain.ddxoftInstance.btn!(1);
-                    mouseUpAction = () => DdxoftMain.ddxoftInstance.btn(2);
-                    break;
-
-                default:
-                    mouseDownAction = () => mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-                    mouseUpAction = () => mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                case "Release":
+                    keybd_event(keyCode, 0, KEYEVENTF_KEYUP, 0);
                     break;
             }
-
-            mouseDownAction.Invoke();
-            await Task.Delay(clickDelayMilliseconds);
-            mouseUpAction.Invoke();
-
-            LastClickTime = DateTime.UtcNow;
-        }
-
-        public static void DoAntiRecoil()
-        {
-            int timeSinceLastClick = Math.Abs(DateTime.UtcNow.Millisecond - LastAntiRecoilClickTime);
-
-            if (timeSinceLastClick < Dictionary.AntiRecoilSettings["Fire Rate"])
-            {
-                return;
-            }
-
-            int xRecoil = (int)Dictionary.AntiRecoilSettings["X Recoil (Left/Right)"];
-            int yRecoil = (int)Dictionary.AntiRecoilSettings["Y Recoil (Up/Down)"];
-
-            switch (Dictionary.dropdownState["Mouse Movement Method"])
-            {
-                case "SendInput":
-                    SendInputMouse.SendMouseCommand(MOUSEEVENTF_MOVE, xRecoil, yRecoil);
-                    break;
-
-                case "LG HUB":
-                    LGMouse.Move(0, xRecoil, yRecoil, 0);
-                    break;
-
-                case "Razer Synapse (Require Razer Peripheral)":
-                    RZMouse.mouse_move(xRecoil, yRecoil, true);
-                    break;
-
-                case "ddxoft Virtual Input Driver":
-                    DdxoftMain.ddxoftInstance.movR!(xRecoil, yRecoil);
-                    break;
-
-                default:
-                    mouse_event(MOUSEEVENTF_MOVE, (uint)xRecoil, (uint)yRecoil, 0, 0);
-                    break;
-            }
-
-            LastAntiRecoilClickTime = DateTime.UtcNow.Millisecond;
         }
 
         public static void MoveCrosshair(int detectedX, int detectedY)
         {
-            int halfScreenWidth = (int)ScreenWidth / 2;
-            int halfScreenHeight = (int)ScreenHeight / 2;
+            // Existing logic...
 
-            int targetX = detectedX - halfScreenWidth;
-            int targetY = detectedY - halfScreenHeight;
-
-            double aspectRatioCorrection = ScreenWidth / ScreenHeight;
-
-            int MouseJitter = (int)Dictionary.sliderSettings["Mouse Jitter"];
-            int jitterX = MouseRandom.Next(-MouseJitter, MouseJitter);
-            int jitterY = MouseRandom.Next(-MouseJitter, MouseJitter);
-
-            Point start = new(0, 0);
-            Point end = new(targetX, targetY);
-            Point control1 = new(start.X + (end.X - start.X) / 3, start.Y + (end.Y - start.Y) / 3);
-            Point control2 = new(start.X + 2 * (end.X - start.X) / 3, start.Y + 2 * (end.Y - start.Y) / 3);
-            Point newPosition = CubicBezier(start, end, control1, control2, 1 - Dictionary.sliderSettings["Mouse Sensitivity (+/-)"]);
-
-            targetX = Math.Clamp(targetX, -150, 150);
-            targetY = Math.Clamp(targetY, -150, 150);
-
-            targetY = (int)(targetY * aspectRatioCorrection);
-
-            targetX += jitterX;
-            targetY += jitterY;
-
-            switch (Dictionary.dropdownState["Mouse Movement Method"])
+            if (AutoTriggerEnabled)
             {
-                case "SendInput":
-                    SendInputMouse.SendMouseCommand(MOUSEEVENTF_MOVE, newPosition.X, newPosition.Y);
-                    break;
-
-                case "LG HUB":
-                    LGMouse.Move(0, newPosition.X, newPosition.Y, 0);
-                    break;
-
-                case "Razer Synapse (Require Razer Peripheral)":
-                    RZMouse.mouse_move(newPosition.X, newPosition.Y, true);
-                    break;
-
-                case "ddxoft Virtual Input Driver":
-                    DdxoftMain.ddxoftInstance.movR!(newPosition.X, newPosition.Y);
-                    break;
-
-                default:
-                    mouse_event(MOUSEEVENTF_MOVE, (uint)newPosition.X, (uint)newPosition.Y, 0, 0);
-                    break;
-            }
-
-            if (Dictionary.toggleState["Auto Trigger"])
-            {
-                Task.Run(DoTriggerClick);
+                PerformKeyAction(TargetDetectedKey, TargetDetectedAction);
             }
         }
     }
